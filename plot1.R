c662e3f0fb0aa3e2ef1c19060cfc9ab443e69c37
AHYEEK <ahyeek@gmail.com>
2016-03-26 21:22:18 +0800

    Add plot1.R, plot1.png

--- plot1.R
+++ plot1.R
@@ -1,3 +1,16 @@
 ### Exploratory Data Analysis
 ### plot1.R
 
+# Read in the CSV data.
+fn_data = "household_power_consumption.txt"
+my_data = read.table(fn_data, header=TRUE, sep=";", stringsAsFactors = FALSE, dec=".")
+subset_mydata =  my_data[my_data$Date %in% c("1/2/2007","2/2/2007") ,]
+names(subset_mydata)
+
+# Preparing graph data.
+global_active_power = as.numeric(subset_mydata$Global_active_power)
+
+# Plot area.
+png("plot1.png", width=480, height=480)
+hist(global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", font.lab=2)
+dev.off()
